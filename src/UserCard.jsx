8b307dda859684cc5a150dca7dbfd37b200133f1
muna-adam <maymuunaadan252@gmail.com>
2026-07-06 12:14:22 +0300

    Add UserCard component

---
+++ src/UserCard.jsx
@@ -0,0 +1,15 @@
+
+
+const UserCard = () => {
+    return (
+        <div>
+    <h1>Amina Hassan</h1>
+    <p>amina.hassan@gmail.com</p>
+    </div>  
+        )
+}
+
+  
+    
+
+export default UserCard;
