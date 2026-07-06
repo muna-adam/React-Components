3e8413f5a8c891182fe811980c16c6fd6c5e8d62
muna-adam <maymuunaadan252@gmail.com>
2026-07-06 12:19:31 +0300

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// function App(){
+//   return (
+//     <Muna/>
+//   )
+// }
+
+import UserCard from "./UserCard";
+
+function App () {
+  return (
+    <UserCard/>
+  );
+}
+
+
+
+export default App;
