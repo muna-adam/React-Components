bd46c176016e1654362acd2a0c9d11e16876b6fc
muna-adam <maymuunaadan252@gmail.com>
2026-07-06 17:02:13 +0300

    Exercise one and two

---
+++ src/UserCard-ex-one.jsx
@@ -0,0 +1,11 @@
+
+const UserCard = () => {
+  return (
+    <div>
+      <h2>Amina Hassan</h2>
+      <p>aamina.hasan@example.com</p>
+    </div>
+  );
+};
+
+export default UserCard;
---
+++ src/UserCard-ex-three.jsx
@@ -0,0 +1,14 @@
+
+const UserCard = ({currentName, currentEmail}) => {
+    return (
+        <div>
+            <h3>Your Name is: {currentName}</h3>         
+            <p>Your Email is: {currentEmail}</p>         
+        </div>  
+        )
+}
+
+  
+    
+
+export default UserCard;
